e9492d2c4b748f76f97069761e9b002cc5da5e30
AndhikaPrasetyo <andhikapras15@gmail.com>
2022-03-01 09:22:30 +0700

    logo

--- src/App.js
+++ src/App.js
@@ -1,39 +1,43 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import { Route, Routes, Link, useLocation } from "react-router-dom";
-import Home from "./pages/home";
-import Resep from "./pages/resep";
+
 
 function App() {
-  let loc = useLocation();
-  // useLocation gunanya untuk mendapatkan data dimana user berada
-  console.log(loc);
-
-  const activeClassName = (path) => {
-    return loc.pathname === path ? "active" : "";
-  };
-
   return (
-    <div>
-      <div className="p-3 d-flex justify-content-center bg-primary">
-        <Link to="/" className={`me-2 ${activeClassName("/")}`}>
-          Home
-        </Link>
-        <Link to="/resep" className={"me-2 " + activeClassName("/resep")}>
-          Resep
-        </Link>
+    <div className="gradient-container">
+      {/* <!-- Header Start --> */}
+      <div className="header-container d-flex justify-content-space-between">
+        <div className="logo">LOGO</div>
+        <div className="menu-container">
+          <div className="text-primary garis">Home<i className="fa-solid fa-bars"></i></div>
+          <div>About</div>
+          <div>contact us</div>
+        </div>
       </div>
-      <div>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/resep" element={<Resep />} />
-        </Routes>
-        {/* <Resep /> */}
-        {/* <Home /> */}
+      {/* Header End */}
+      {/* jumbotron 1 start */}
+      <div className="jumbotron-1-container pd-container-hz">
+        <div className="kiri">
+          <div className="title">New E-commerce App best for You</div>
+          <div className="subtitle">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Amet sed
+            vulputate vitae velit dictum cursus amet. Turpis donec ut velit
+            quis. Cursus commodo, eget urna, sapien amet.
+          </div>
+          <div className="button-container">
+            <button>Playstore</button>
+            <button>app store</button>
+          </div>
+        </div>
+        <div className="kanan">
+          <div className="image-container">
+            <img src="./assets/Mockup.svg" alt="image1" height="100%" width="100%" />
+          </div>
+        </div>
       </div>
     </div>
   );
 }
 
-// export default hanya bisa diexport sekali pada satu file
+
 export default App;
