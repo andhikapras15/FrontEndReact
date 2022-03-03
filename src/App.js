ecf8309170ec407cb3c03d84de3ee3d59c6095d6
AndhikaPrasetyo <andhikapras15@gmail.com>
2022-03-03 08:38:23 +0700

    day4 frontend day7

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./App.css";
-import { Route, Routes, Link, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Resep from "./pages/resep";
+import Login from "./pages/login";
+import ResepDetail from "./pages/resepDetail";
+import Topics from "./pages/topics";
+import NotFound from "./pages/notfound";
+import Header from "./components/header";
 
 function App() {
-  let loc = useLocation();
-  // useLocation gunanya untuk mendapatkan data dimana user berada
-  console.log(loc);
-
-  const activeClassName = (path) => {
-    return loc.pathname === path ? "active" : "";
-  };
-
   return (
     <div>
-      <div className="p-3 d-flex justify-content-center bg-primary">
-        <Link to="/" className={`me-2 ${activeClassName("/")}`}>
-          Home
-        </Link>
-        <Link to="/resep" className={"me-2 " + activeClassName("/resep")}>
-          Resep
-        </Link>
-      </div>
+      <Header />
       <div>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/resep" element={<Resep />} />
+          <Route path="/resep/:id" element={<ResepDetail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/topics/*" element={<Topics />} />
+          <Route path="*" element={<NotFound />} />
         </Routes>
         {/* <Resep /> */}
         {/* <Home /> */}
---
+++ src/components/button.jsx
@@ -0,0 +1,47 @@
+import "./buttons.css";
+export const ButtonPrimary = (props) => {
+    let className = props.className || "";
+    let style = props.style || {};
+
+    return (
+        <button
+            style={{
+                height: "30px",
+                width: "100px",
+                margin: "10px 0px",
+                color: "white",
+                backgroundColor: "magenta",
+                border: "none",
+                fontSize: "20px",
+
+                ...style,
+            }}
+            className={`tes ${className}`}
+        >
+            {props.name}
+        </button>
+    );
+};
+
+export const ButtonSecondary = (props) => {
+    //   console.log(props);
+    return (
+        <button
+            style={{
+                height: "30px",
+                width: "100px",
+                margin: "10px 0px",
+                color: "white",
+                backgroundColor: "coral",
+                border: "none",
+                fontSize: "20px",
+                ...props.style,
+            }}
+            className={`tombol ${props.className}`}
+            disabled={props.disabled}
+            onClick={props.onClick}
+        >
+            {props.children}
+        </button>
+    );
+};
